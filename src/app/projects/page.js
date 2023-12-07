ab7bb4532046f1b944984ff5ddf547ea48409534
toriq <thoriqahmad17@gmail.com>
2023-12-08 06:57:49 +0700

    comiit

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -9,7 +9,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-const Page = () => {
+const page = () => {
   const [DataImg, setDataImg] = useState(projects);
   const [Choice, setChoice] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
@@ -131,4 +131,4 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default page;
